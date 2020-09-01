7ee2c50add793aff0560a1b92687ee5c18b8c379
Arindam Chaudhuri <ksharindam@gmail.com>
2020-09-01 23:03:47 +0530

    grayscale filter changed to use luminance, and color utils optimized

--- src/transform.cpp
+++ src/transform.cpp
@@ -10,8 +10,6 @@ This file is a part of photoquick program, which is GPLv3 licensed
 #include <QRect>
 #include <cmath>
 
-#define MIN(a,b) (((a)<(b))?(a):(b))
-#define MAX(a,b) (((a)>(b))?(a):(b))
 
 //Mean for Isometric mode Un-tilt (PerspectiveTransform)
 QPoint meanx2(QPoint p1, QPoint p2, QPoint p3, QPoint p4)
